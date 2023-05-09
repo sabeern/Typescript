0ba5c43278b59dd515fea7bb16483fce2bc1d94e
sabeern <sabeerneyyan@gmail.com>
2023-05-09 11:05:15 +0530

    Added array and object

--- sandbox.js
+++ sandbox.js
@@ -8,3 +8,20 @@ var circ = function (diameter) {
     console.log(diameter * Math.PI);
 };
 circ(5);
+var arr1 = ["orange", "apple", "banana"];
+var arr2 = [45, 3, 6, 9];
+arr1[0] = "mango";
+//arr1[1] = 30;
+arr2.push(35);
+//arr2.push('anees');
+var mixed = ["orange", 2, 3, "grapes"];
+mixed[0] = 4;
+var obj = { name: "sabeer", age: 23, place: "malappuram" };
+obj.name = "anees";
+//obj.age = '3';
+//obj.mark = 33;
+obj = {
+    name: "fahiz",
+    age: 21,
+    place: "kondotty",
+};
--- sandbox.ts
+++ sandbox.ts
@@ -13,3 +13,26 @@ const circ = (diameter: number) => {
 };
 
 circ(5);
+
+const arr1 = ["orange", "apple", "banana"];
+const arr2 = [45, 3, 6, 9];
+
+arr1[0] = "mango";
+//arr1[1] = 30;
+arr2.push(35);
+//arr2.push('anees');
+
+const mixed = ["orange", 2, 3, "grapes"];
+
+mixed[0] = 4;
+
+let obj = { name: "sabeer", age: 23, place: "malappuram" };
+
+obj.name = "anees";
+//obj.age = '3';
+//obj.mark = 33;
+obj = {
+  name: "fahiz",
+  age: 21,
+  place: "kondotty",
+};
