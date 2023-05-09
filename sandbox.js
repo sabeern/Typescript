5828f5f9559b170f807602732a7449b786f4296d
sabeern <sabeerneyyan@gmail.com>
2023-05-09 10:53:06 +0530

    Initial file of typescript

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+var character = "sabeer";
+var age = 20;
+var isPassed = true;
+character = "anees";
+age = 23;
+isPassed = false;
+var circ = function (diameter) {
+    console.log(diameter * Math.PI);
+};
+circ(5);
---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+let character = "sabeer";
+let age = 20;
+let isPassed = true;
+
+character = "anees";
+
+age = 23;
+
+isPassed = false;
+
+const circ = (diameter: number) => {
+  console.log(diameter * Math.PI);
+};
+
+circ(5);
